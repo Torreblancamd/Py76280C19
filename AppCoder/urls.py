9b1d424a759f4fe6be714a23883268c9a6b0754a
Maximiliano Torreblanca <torreblancamd@gmail.com>
2025-07-01 09:17:07 -0300

    v1

---
+++ AppCoder/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+from . import views
+
+urlpatterns=[
+    path('cursos' , views.cursos),
+    path("alta_curso/<str:nombre>" , views.alta_curso),
+    path("profesores", views.profesores) 
+      
+] 
---
+++ AppCoder/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render # type: ignore
+from django.http import HttpResponse
+from AppCoder.models import Curso
+from django.template import loader
+# Create your views here.
+
+def cursos(request):
+    cursos = Curso.objects.all()
+    dicc = {"cursos":cursos}
+    plantilla = loader.get_template("cursos.html")
+    documento = plantilla.render(dicc)
+    return HttpResponse( documento)
+
+
+
+def alta_curso(request, nombre):
+    curso = Curso(nombre=nombre , camada=99999)
+    curso.save()
+    msj = f"Se guardo el curso {curso.nombre} {curso.camada}"
+    return HttpResponse(msj)
+
+
+
+def profesores(request):
+    return render(request, "profesores.html")
+
+
+
